6868b898628320e404f347c3247156368a92446a
unknown <patrick.andrade1904@gmail.com>
2023-03-29 23:13:13 -0300

    Implementado feed

--- src/controladores/postagens.js
+++ src/controladores/postagens.js
@@ -95,8 +95,62 @@ const comentar = async (req, res) => {
     }
 }
 
+const feed = async (req, res) => {
+    const {id} = req.usuario
+    const {offset} = req.query
+
+    const o = offset ? offset : 0;
+
+    try {
+        const postagens = await knex('postagens')
+        .where('usuario_id', '!=', id)
+        .limit(10)
+        .offset(o);
+
+        if (postagens.length === 0) {
+            return res.status(200).json(postagens)
+        }
+
+        for (const postagem of postagens) {
+            // usuario 
+            const usuario = await knex('usuarios')
+            .where({id: postagem.usuario_id})
+            .select('imagem', 'username', 'verificado')
+            .first()
+            postagem.usuario = usuario
+
+            // fotos 
+            const fotos = await knex('postagem_fotos')
+            .where({postagem_id: postagem.id})
+            .select('imagem')
+            postagem.fotos = fotos
+
+            // curtidas
+            const curtidas = await knex('postagem_curtidas')
+            .where({postagem_id: postagem.id})
+            .select('usuario_id')
+            postagem.curtidas = curtidas.length
+
+            // curtida por mim 
+            postagem.curtidoPorMim = curtidas.find(curtida => curtida.usuario_id === id) ? true : false
+
+            // comentarios
+            const comentarios = await knex('postagem_comentarios')
+            .leftJoin('usuarios', 'usuarios.id', 'postagem_comentarios.usuario_id')
+            .where({postagem_id: postagem.id})
+            .select('usuarios.username', 'postagem_comentarios.texto')
+            postagem.comentarios = comentarios
+        }
+
+        return res.status(200).json(postagens)
+    } catch (error) {
+        return res.status(400).json(error.message)
+    }
+}
+
 module.exports = {
     novaPostagem,
     curtir,
-    comentar
+    comentar,
+    feed
 }
--- src/rotas.js
+++ src/rotas.js
@@ -21,6 +21,7 @@ rotas.put('/perfil', usuarios.atualizarPerfil)
 
 // postagens
 rotas.post('/postagens', postagens.novaPostagem)
+rotas.get('/postagens', postagens.feed)
 rotas.post('/postagens/:postagemId/curtir', postagens.curtir)
 rotas.post('/postagens/:postagemId/comentar', postagens.comentar)
 
